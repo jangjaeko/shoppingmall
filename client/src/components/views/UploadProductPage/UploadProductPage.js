503fbc0db7002274b655e2513e898e06fc2e5bf6
jangjaeko <jangjaeko45@gmail.com>
2021-02-08 19:50:47 +0900

    upload

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,9 +1,22 @@
 import React ,{useState}from 'react'
 import {Typography, Button, Form , Input} from 'antd';
+import FileUpload from '../../utils/FileUpload';
+import Axios from 'axios';
 
 
 const {TextArea} = Input
-function UploadProductPage() {
+
+const Continents = [
+    {key :1, value : "Africa"},
+    {key :2, value : "Europe"},
+    {key :3, value : "Asia"},
+    {key :4, value : "North America"},
+    {key :5, value : "South America"},
+    {key :6, value : "Australia"},
+    {key :7, value : "Antarctica"}    
+]
+
+function UploadProductPage(props) {
 
     const [Title, setTilte] = useState("")
     const [Description ,setDescription] = useState("")
@@ -19,13 +32,47 @@ function UploadProductPage() {
     const priceChageHandler = (event)=>{
         setPrice(event.currentTarget.value)
     }
+    const continentChagneHandler =(event) =>{
+        setContinet(event.currentTarget.value)
+    }
+    const updateImages = (newImages) => {
+        setImages(newImages)
+    }
+    const submitHandler=(event) =>{
+        event.preventDefault();
+        
+        if(!Title ||!Description || !Price || !Continent || !Images){
+            return alert("모든 값을 넣어주셔야 합니다.")
+        }
+
+        //서버에 채운 값들을 request로 보낸다.
+        const body ={
+            //로그인된 사람의 아이디를 넣어주는거
+            writer : props.user.userData._id,
+            title : Title,
+            description : Description,
+            price : Price,
+            images : Images,
+            continents : Continent
+        }
+        Axios.post('/api/product',body)
+            .then(response=>{
+                if(response.data.success){
+                    alert('상품업로드에 성공 했습니다.')
+                    props.history.push('/')
+                }
+                else {
+                    alert('상품업로드에 실패 했습니다.')
+                }
+            })
+    }
     return (
         <div style={{maxWidth:'700px', margin:"2rem auto"}}>
             <div style= {{textAlign: 'center', marginBottom:'2rem'}}>
                 <Typography level={2}>여행 상품 업로드</Typography>
             </div>
-            <Form>
-                {/*  dropzone*/ }
+            <Form onSubmit= {submitHandler}> 
+            <FileUpload  refreshFunction={updateImages} /> 
             <br/>
             <br/>
             <label>이름</label>
@@ -40,12 +87,14 @@ function UploadProductPage() {
             <Input type="number" onChange = {priceChageHandler} value = {Price} />
             <br/>
             <br/>
-            <select>
-                <option></option>
+            <select onChange = {continentChagneHandler} value ={Continent} >
+                {Continents.map(item =>(
+                    <option key = {item.key} value={item.key} >{item.value}</option>    
+                ))}
             </select>
             <br/>
             <br/>
-            <Button>
+            <Button type="submit">
                 확인
             </Button>
             </Form>
